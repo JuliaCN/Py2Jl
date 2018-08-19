2b0e555fc1b2cc215d07521d0769b76175f0d5e8
thautwarm <twshere@outlook.com>
2018-08-19 20:33:12 +0800

    add dev guide

---
+++ develop-guide.md
@@ -0,0 +1,75 @@
+
+## Python ASDL
+
+https://github.com/python/cpython/blob/master/Parser/Python.asdl
+With this you can get clear understanding of python's ast.
+
+
+- Additional suggestions for Python: `astpretty`, `ast.parse`
+
+```
+integrated-terminal> from astpretty import pprint as pp
+integrated-terminal> from ast import parse as p
+integrated-terminal> from toolz import compose
+integrated-terminal> q = compose(pp, p)
+integrated-terminal> q("for a, b in s : a")
+Module(
+    body=[
+        For(
+            lineno=1,
+            col_offset=0,
+            target=Tuple(
+                lineno=1,
+                col_offset=4,
+                elts=[
+                    Name(lineno=1, col_offset=4, id='a', ctx=Store()),
+                    Name(lineno=1, col_offset=7, id='b', ctx=Store()),
+                ],
+                ctx=Store(),
+            ),
+            iter=Name(lineno=1, col_offset=12, id='s', ctx=Load()),
+            body=[
+                Expr(
+                    lineno=1,
+                    col_offset=15,
+                    value=Name(lineno=1, col_offset=15, id='a', ctx=Load()),
+                ),
+            ],
+            orelse=[],
+        ),
+    ],
+)
+```
+
+
+- Additional suggestions from Julia:
+
+```
+julia> macro q(a) dump(a) end
+julia> @q while x
+          x + 1
+          continue 2
+          end
+Expr
+  head: Symbol while
+  args: Array{Any}((2,))
+    1: Symbol x
+    2: Expr
+      head: Symbol block
+      args: Array{Any}((4,))
+        1: LineNumberNode
+          line: Int64 2
+          file: Symbol REPL[38]
+        2: Expr
+          head: Symbol call
+          args: Array{Any}((3,))
+            1: Symbol +
+            2: Symbol x
+            3: Int64 1
+        3: LineNumberNode
+          line: Int64 3
+          file: Symbol REPL[38]
+        4: Expr
+          head: Symbol continue
+          args: Array{Any}((0,))
+```
--- src/ASTGen.jl
+++ src/ASTGen.jl
@@ -2,13 +2,52 @@ module ASTGen
 
 using MLStyle
 
+macro not_implemented_yet()
+    @error "not_implemented_yet"
+end
+
+function ret_nil(node)
+    Expr(:block, node, nothing)
+end
+
+function annotate(sym, ty)
+    Expr(:(::), sym, ty)
+end
+
+function assign(target, value)
+    Expr(:(=), target, value)
+end
+
+function (<|)(f, arg)
+    f(arg)
+end
+
+function gather(args)
+    Expr(:block, args...)
+end
+
+function call(fn, args)
+    Expr(:call, fn, args...)
+end
+
+function as_global(names)
+    Expr(:global, names...)
+end
+
+function break!()
+    Expr(:break)
+end
+
+function continue!()
+    Expr(:continue)
+end
 
 function to_ast(filename, python :: Dict)
 
-
     @def apply begin
-    (num :: Number)  => num
-    (str :: String)  => str
+
+    (num :: Number) => num
+    (str :: String) => str
     (nil :: Nothing) => nil
 
     Dict(:class => "Module", :name => name, :body => body) =>
@@ -16,50 +55,110 @@ function to_ast(filename, python :: Dict)
         Expr(:module, true, map(apply, body)...)
 
 
-    Dict(:class   => fn_ty_name,
-         :args    =>
-             Dict(:vararg      => vararg,
-                  :kwonlyargs  => kwonlyargs,
+    Dict(:class => fn_ty_name,
+         :args =>
+             Dict(:vararg => vararg,
+                  :kwonlyargs => kwonlyargs,
                   :kw_defaults => kw_defaults,
-                  :kwarg       => kwarg,
-                  :lineno      => lineno, :colno => colno),
+                  :kwarg => kwarg,
+                  :lineno => lineno, :colno => colno),
          :body = body) in fn_ast =>
          begin
-                if kwarg === nothing && isempty(kwonlyargs)
-                   if is_empty(kw_defaults) && is_empty(defaults)
-                        args = map(apply, args)
-                        block = apply(body)
-                        if fn_ty_name == "Lambda"
-                            Expr(:function, Expr(:tuple, args...), Expr(:block, body))
-                        else
+            if kwarg === nothing && isempty(kwonlyargs)
+               if is_empty(kw_defaults) && is_empty(defaults)
 
-                            decorator_list = fn_ast[:decorator_list]
-                            fn_name = Symbol(fn_ast[:name])
-                            init = Expr(:function, Expr(:call, fn_name, args...), Expr(:block, body))
-                            reduce(decorator_list, init=init) do last, decorator
-                                Expr(:(=), fn_name, Expr(:let, Expr(:block), Expr(:call, apply(decorator), last)))
-                            end
+                    args = map(apply, args)
+                    block = gather <| map(apply, body)
 
-                        end
-                   else
-                        @error "not impl yet"
-                   end
-                else
-                    @error "not impl yet"
-                end
+                    if fn_ty_name == "Lambda"
+                        Expr(:function, Expr(:tuple, args...), Expr(:block, body))
+                    else
+                        decorator_list = fn_ast[:decorator_list]
+                        fn_name = Symbol(fn_ast[:name])
+                        init = Expr(:function, Expr(:call, fn_name, args...), Expr(:block, body))
+                        reduce(decorator_list, init=init) do last, decorator
+                            Expr(:(=), fn_name, Expr(:let, Expr(:block), Expr(:call, apply(decorator), last)))
+                        end |>
+                        ret_nil
+                    end
+               else
+                    @not_implemented_yet
+               end
+            else
+                @not_implemented_yet
+            end
          end
 
-    Dict(:class => "FunctionDef",
-         :name => name,
-         :args = args,
-         :body = body,
-         :decorator_list => decorator_list,
-         :returns => returns) =>
-                1
+    Dict(:class => "Assign",
+         :targets => targets,
+         :value => value) =>
 
+         reduce(targets, init = apply(value)) do last, target
+            Expr(:(=), target, last)
+         end |> ret_nil
+
+    Dict(:class => "AugAssign",
+         :target => target,
+         :op => op,
+         :value => value) =>
+         @not_implemented_yet
+
+    Dict(:class => "AnnAssign",
+         :target => target,
+         :annotation => annotation,
+         :value => value) =>
+         annotate(apply(target), apply(annotation)) |>
+         target -> assign(target, value)            |>
+         ret_nil
+
+    Dict(:class => "For",
+         :target => target,
+         :iter => iter,
+         :body => body,
+         :or_else => or_else) =>
+         if isempty(or_else)
+            Expr(:for,
+                 assign(apply(target), apply(iter)),
+                 gather <| map(apply, body))
+         else
+            # a efficient implementation of `for...else...` is necessary.
+            @not_implemented_yet
+         end
+
+    Dict(:class => "While",
+         :test  => test,
+         :body => body,
+         :or_else => or_else) =>
+         if isempty(or_else)
+            Expr(:while,
+                 apply(test),
+                 gather <| map(apply, body))
+         else
+            @not_implemented_yet
+         end
+
+    Dict(:class => "With") => @not_implemented_yet
+
+    Dict(:class => "ClassDef") => @not_implemented_yet
+
+    Dict(:class => "Raise")   => @not_implemented_yet
+
+    Dict(:class => "Import") => @not_implemented_yet
+
+    Dict(:class => "ImportFrom") => @not_implemented_yet
+
+    Dict(:class => "Global",
+         :names = names) => as_global(map(apply, names))
+    Dict(:class => "Pass") => nothing
+
+    Dict(:class => "Break") => break!()
+
+    Dict(:class => "Continue") => continue!()
 
 
     end
+
+
 
     apply(python)
 
