0ca52eba660e700553bd68f32d1389e2c58c4854
thautwarm <twshere@outlook.com>
2019-02-01 22:24:33 +0800

    update figure

--- demo.jl
+++ demo.jl
@@ -12,7 +12,8 @@ result = sum_by(lambda x: x * 10, [1, 2, 3])
 @info :interops sum_by(x -> 2x, [1, 2, 3])
 
 
-@info transpiled_to_julia py2jl("mymod", """
+@info :transpiled_to_julia py2jl("mymod",
+"""
 def sum_by(f, seq):
     s = 0
     for e in seq:
