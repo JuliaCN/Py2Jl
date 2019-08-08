75e2e7f0e8312fed5d384c57f9be2b54b9514758
thautwarm <twshere@outlook.com>
2019-08-09 01:31:35 +0800

    update dependencies

--- test/test_case1.jl
+++ test/test_case1.jl
@@ -9,8 +9,23 @@ def sumBy(f, seq):
 
 result = sumBy(lambda x: 1 + x, [100, 200])
     """
-    @testset "transpiled codes run in the pure Julia runtime" begin
+    @testset "transpiled codes run in the pure Julia runtime[1]" begin
         @test result == 302
         @test sumBy(identity, [1, 2, 3]) === 6
     end
+
+    py2jl"""
+def sum_by(f, seq):
+    s = 0
+    for e in seq:
+        s = s + f(e)
+    return s
+result = sum_by(lambda x: x * 10, [1, 2, 3])
+    """
+
+     @testset "transpiled codes run in the pure Julia runtime[2]" begin
+        @test result == 60
+        @test sum_by(identity, [1, 2, 3]) === 6
+    end
+
 end
